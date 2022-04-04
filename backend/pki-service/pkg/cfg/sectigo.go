0b7d559a667ae384c793c9e0eeb58ac14f2612c4
Florian Ritterhoff <florian@ritterhoff.org>
2022-04-04 18:03:57 +0200

    docs: add missing docs for sectigo cfg

--- backend/pki-service/pkg/cfg/sectigo.go
+++ backend/pki-service/pkg/cfg/sectigo.go
@@ -1,5 +1,6 @@
 package cfg
 
+// SectigoConfiguration handles different configuration properties for the sectigo client
 type SectigoConfiguration struct {
 	User           string `mapstructure:"sectigo_user"`
 	Password       string `mapstructure:"sectigo_password"`
