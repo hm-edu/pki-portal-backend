2589fc02d0852c600a0cb57cb64d90ab36e438b6
Florian Ritterhoff <florian@ritterhoff.org>
2022-08-28 14:28:06 +0200

    ci: don't update portal-common

--- helper/update.sh
+++ helper/update.sh
@@ -2,7 +2,9 @@
 
 for x in backend/*/ ; do
     pushd $x
+    OLD=$(cat go.mod | grep "portal-common v0.0.0")
     go get -u ./...
+    sed -i 's@^.*portal-common v0.0.0.*$@'"$OLD"'@' go.mod
     go mod tidy 
     popd
 done
