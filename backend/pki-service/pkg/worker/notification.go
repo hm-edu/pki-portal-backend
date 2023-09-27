fc0cdaaa48de748de0256c11a3727f499e96df27
Florian Ritterhoff <florian.ritterhoff@hm.edu>
2023-09-27 14:18:08 +0200

    add tests and fix some minor bugs

--- backend/pki-service/pkg/worker/notification.go
+++ backend/pki-service/pkg/worker/notification.go
@@ -23,41 +23,60 @@ type Notifier struct {
 	Db        *ent.Client
 }
 
-// Notify triggers an email for each
-func (w *Notifier) Notify(logger *zap.Logger) error {
+type certificateItem struct {
+	cert   *ent.Certificate
+	domain string
+}
+
+func (w *Notifier) loadCertificates() (map[int]certificateItem, error) {
 
 	domains, err := w.Db.Domain.Query().All(context.Background())
 	if err != nil {
-		return err
+		return nil, err
 	}
-	doneCertificates := make(map[int]struct{})
+	doneCertificates := make(map[int]certificateItem)
 	for _, d := range domains {
 		certificate, err := w.Db.Certificate.Query().WithDomains().Where(certificate.HasDomainsWith(domain.FqdnEQ(d.Fqdn)),
 			certificate.StatusNEQ(certificate.StatusRevoked),
 			certificate.StatusNEQ(certificate.StatusInvalid)).Order(ent.Desc(certificate.FieldNotAfter)).Limit(1).All(context.Background())
 		if err != nil {
-			return err
+			return nil, err
 		}
 		if len(certificate) == 0 {
 			continue
 		}
 		if certificate[0].NotAfter.Before(time.Now().AddDate(0, 0, 30)) && certificate[0].NotAfter.After(time.Now()) {
+
 			days := time.Until(certificate[0].NotAfter).Hours() / 24
 			if _, ok := doneCertificates[certificate[0].ID]; !ok && int(days)%7 == 0 {
-				// TODO: send notification
-				logger.Info(fmt.Sprintf("Certificate for %s expires in %d days, sending notification.", d.Fqdn, int(days)))
-				certDomains := certificate[0].Edges.Domains[0].Fqdn
-				for _, x := range certificate[0].Edges.Domains[1:] {
-					certDomains = fmt.Sprintf("%s, %s", certDomains, x.Fqdn)
-				}
-				to := []string{strings.Split(*certificate[0].IssuedBy, " ")[0]}
-				if w.MailTo != "" {
-					to = []string{w.MailTo}
-				}
-				if w.MailToBcc != "" && w.MailToBcc != to[0] {
-					to = append(to, w.MailToBcc)
-				}
-				err = smtp.SendMail(fmt.Sprintf("%s:%d", w.MailHost, w.MailPort), nil, w.MailFrom, to, []byte(fmt.Sprintf(`
+				doneCertificates[certificate[0].ID] = certificateItem{cert: certificate[0], domain: d.Fqdn}
+			}
+		}
+	}
+	return doneCertificates, err
+}
+
+// Notify triggers an email for each
+func (w *Notifier) Notify(logger *zap.Logger) error {
+	doneCertificates, err := w.loadCertificates()
+	if err != nil {
+		return err
+	}
+	for _, certificate := range doneCertificates {
+		days := time.Until(certificate.cert.NotAfter).Hours() / 24
+		logger.Info(fmt.Sprintf("Certificate for %s expires in %d days, sending notification.", certificate.domain, int(days)))
+		certDomains := certificate.cert.Edges.Domains[0].Fqdn
+		for _, x := range certificate.cert.Edges.Domains[1:] {
+			certDomains = fmt.Sprintf("%s, %s", certDomains, x.Fqdn)
+		}
+		to := []string{strings.Split(*certificate.cert.IssuedBy, " ")[0]}
+		if w.MailTo != "" {
+			to = []string{w.MailTo}
+		}
+		if w.MailToBcc != "" && w.MailToBcc != to[0] {
+			to = append(to, w.MailToBcc)
+		}
+		err = smtp.SendMail(fmt.Sprintf("%s:%d", w.MailHost, w.MailPort), nil, w.MailFrom, to, []byte(fmt.Sprintf(`
 				From: PKI <%s>
 				To: %s
 				Subject: Infomationen zu Zertifikatsablauf %s
@@ -74,13 +93,12 @@ func (w *Notifier) Notify(logger *zap.Logger) error {
 
 				Mit freundlichen Grüßen,
 				Ihre Zentrale IT
-				`, w.MailFrom, strings.Split(*certificate[0].IssuedBy, " ")[0], d.Fqdn, d.Fqdn, certificate[0].NotAfter.Format("02.01.2006"), certDomains)))
-				if err != nil {
-					logger.Error("Error sending mail", zap.Error(err))
-				}
-				doneCertificates[certificate[0].ID] = struct{}{}
-			}
+				`, w.MailFrom, strings.Split(*certificate.cert.IssuedBy, " ")[0], certificate.domain, certificate.domain, certificate.cert.NotAfter.Format("02.01.2006"), certDomains)))
+		if err != nil {
+			logger.Error("Error sending mail", zap.Error(err))
 		}
+
 	}
+
 	return nil
 }
